1fda866638338a6e233d90ea3d82a1e21e0d0680
vishalreddy1005 <vishalreddy1005@gmail.com>
2026-03-15 18:14:56 +0530

    package update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  plugins: { tailwindcss: {}, autoprefixer: {} },
 };
-
 export default config;
